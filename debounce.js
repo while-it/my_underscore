465ef17b782c0dfb04442893ad089e87f94dcbd2
python_samllbird <467196574@qq.com>
2021-07-04 20:31:53 +0800

    提交防抖函数

---
+++ debounce.js
@@ -0,0 +1,25 @@
+function debounce(func,wait,immediate){
+    let timeout,result ;
+    var debounced = function(){
+        timeout && clearTimeout(timeout);
+        let _this = this;
+        let _args = arguments;
+        if (immediate){
+            let allow_call = !timeout;
+            timeout = setTimeout(function(){
+                timeout = null
+            },wait);
+            if (allow_call) result =  func.apply(_this,[..._args]);
+        }else{
+            timeout = setTimeout(function (){
+                result = func.apply(_this,[..._args]);
+            },wait);
+        }
+        return result;
+    }
+    debounced.cancle = function (){
+        timeout && clearTimeout(timeout);
+        timeout = null;
+    }
+    return debounced;
+}
